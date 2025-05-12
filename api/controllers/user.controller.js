1bdb8a0b80df7dfe4574cb5791d5df5c23dae088
Ayush Pandey <exploretheuniverse216@gmail.com>
2025-05-13 01:30:49 +0530

    "Correction in Update page"

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,34 +1,49 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
+
+// Test route to verify API is working
 export const test = (req, res) => {
-    res.json({
-      message: 'Api route is working!',
-    });
-  };
+  res.json({
+    message: 'API route is working!',
+  });
+};
+
+// Update user profile
 export const updateUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id)
-      return next(errorHandler(401, 'You can only update your own account!'));
-    try {
-      if (req.body.password) {
-        req.body.password = bcryptjs.hashSync(req.body.password, 10);
-      }
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            avatar: req.body.avatar,
-          },
-        },
-        { new: true }
-      );
-      const { password, ...rest } = updatedUser._doc;
-      res.status(200).json(rest);
-    } 
-    catch (error) {
-      next(error);
+  // Allow only the logged-in user to update their own profile
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, 'You can only update your own account!'));
+  }
+
+  try {
+    const updates = {
+      username: req.body.username,
+      email: req.body.email,
+      avatar: req.body.avatar,
+    };
+
+    // Hash password if provided
+    if (req.body.password) {
+      const hashedPassword = bcryptjs.hashSync(req.body.password, 10);
+      updates.password = hashedPassword;
     }
+
+    // Update user and return new document
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: updates },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+
+    // Exclude password from response
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
