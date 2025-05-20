e8b5671032d14d3a7b229654ed6a09abf8d29b10
GuranshGoyal <guransh31goyal@gmail.com>
2025-05-20 18:11:59 +0530

    complete create listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+
 // import React from 'react'
 // import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // import Home from './pages/Home';
@@ -28,29 +58,3 @@
   
 //   )
 // }
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import About from './pages/About';
-import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute';
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
-        <Route element={<PrivateRoute />}>
-          <Route path="/profile" element={<Profile />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
