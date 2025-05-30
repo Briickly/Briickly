cafc38fd13acc735aac40b8ce2baebbfd10ef29b
Goyalkrishna1 <goyalkrishna366@gmail.com>
2025-05-30 13:00:36 +0530

    complete search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import {useState} from 'react';
 
 export default function Header() {
   const {currentUser} = useSelector(state=>state.user)
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e)=>{
+     e.preventDefault();
+     const urlParams = new URLSearchParams(window.location.search);
+     urlParams.set('searchTerm',searchTerm);
+     const searchQuery = urlParams.toString();
+     navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(()=>{
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if(searchTermFromUrl){
+            setSearchTerm(searchTermFromUrl);
+        }
+  }, [location.search]);
   return (
     <header className='bg-amber-600 shadow-md' >
         <div className='flex justify-between items-center max-w-6xl mx auto p-3'>
@@ -16,10 +34,17 @@ export default function Header() {
         </h1>
         </Link>
         
-        <form className='bg-amber-50 p-3 rounded-lg flex items-center'>
-            <input type="text" placeholder='search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-
+        <form onSubmit = {handleSubmit} className='bg-amber-50 p-3 rounded-lg flex items-center'>
+            <input type="text" 
+            placeholder='search...' 
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value = {searchTerm}
+            onChange = {(e) => setSearchTerm(e.target.value)}
+            
+            />
+            <button>
             <FaSearch className='text-black'/>
+            </button>
 
         </form>
         
