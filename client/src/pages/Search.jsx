35eec23664260dda7f52257bbb713eec40bcdcbe
Goyalkrishna1 <goyalkrishna366@gmail.com>
2025-05-30 17:18:56 +0530

    initialized listing item page and in search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useState,useEffect } from 'react';
+import ListingItem from '../components/ListingItem';
 export default function Search() {
     const navigate = useNavigate();
     const [sidebardata, setSidebardata] = useState({
@@ -15,7 +16,7 @@ export default function Search() {
 
 const[loading, setLoading] = useState(false);
 const [listings, setListings] = useState([]);
-
+console.log(listings);
 useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get('searchTerm');
@@ -195,7 +196,7 @@ const handleChange = (e) => {
               className='border rounded-lg p-3'
             >
               <option value='regularPrice_desc'>Price high to low</option>
-              <option value='regularPrice_asc'>Price low to hight</option>
+              <option value='regularPrice_asc'>Price low to high</option>
               <option value='createdAt_desc'>Latest</option>
               <option value='createdAt_asc'>Oldest</option>
             </select>
@@ -209,6 +210,24 @@ const handleChange = (e) => {
         <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
           Listing results:
         </h1>
+        <div className = "p-7 flex flex-wrap gap-4">
+            {!loading && listings.length === 0 && (
+                <p className = 'text-xl text-slate-700'> No listing found!! </p>
+            )}
+
+            {
+                loading && (
+                    <p className = 'text-xl text-slate-700 text-center w-full'> Loading... </p>
+                )
+            }
+
+            {!loading &&
+            listings &&
+            listings.map((listing) => (
+              <ListingItem key={listing._id} listing={listing} />
+            ))}
+        </div>
+
       </div>
     </div>
   );
