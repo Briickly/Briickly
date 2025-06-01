494e62b10312c6546a8dcfb25149ae2b5e8db2fa
Ayush Pandey <exploretheuniverse216@gmail.com>
2025-06-01 20:37:24 +0530

    Abstract About Page complete

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      ABOUT
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Briickly</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, expedita dicta nemo, amet dolorum soluta excepturi provident sed laboriosam quisquam qui eligendi labore deleniti quod ex atque perferendis ducimus natus! Atque minima a nostrum provident perferendis recusandae incidunt similique assumenda ex consectetur facere veritatis aspernatur nam, nisi quod neque eaque dicta ut sed distinctio temporibus iusto sint tempore. Suscipit facilis maiores ipsa at iusto quisquam impedit recusandae fugit. Architecto earum similique ad vero natus consequatur officia unde ex quam! Magnam facilis autem, illo vel deleniti doloribus. Doloribus eveniet dignissimos atque? Laboriosam laudantium pariatur recusandae commodi rerum. Accusantium dicta a nemo?</p>
+
+      <br />
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam blanditiis voluptatum fuga excepturi consequatur, vel earum minus accusamus molestias ab quasi id praesentium, pariatur odio sint! Corporis voluptatibus veritatis repellat molestias velit nesciunt id. Provident assumenda at illum numquam nemo omnis itaque placeat alias molestias dolorem nostrum quidem, vel ullam, sed ad autem? Dolorem cumque quaerat itaque rem debitis deserunt totam quos dignissimos repellat atque deleniti, repellendus eius praesentium harum velit doloremque placeat sed iusto facere quibusdam voluptas esse. Earum inventore autem molestias iste. Distinctio voluptatibus exercitationem, debitis beatae explicabo dolorem magni nihil tenetur, sit ullam quasi numquam dicta itaque.</p>
+
+      <br />
+
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium maiores libero molestias animi consequuntur autem, cum iure harum adipisci neque aut provident repellendus incidunt aspernatur, nisi, consequatur laboriosam! Eveniet pariatur libero optio voluptatum officiis? Quaerat vel in asperiores, beatae impedit, sequi delectus illum, nam ipsam libero quae exercitationem! Doloremque excepturi odio dolorum, illo dolorem unde porro culpa esse, illum qui corrupti aperiam? Veritatis quibusdam, natus mollitia, ut, hic fuga ducimus necessitatibus quas debitis suscipit aperiam dolorum dolore. Voluptas similique natus corporis harum omnis voluptatem! Autem, porro veniam vel nemo itaque esse pariatur consequuntur doloremque minus optio aperiam impedit fugiat nostrum?
+      </p>
     </div>
   )
 }
