196b4f334c658dfb69ede451fe8700d73a9b773d
Ayush Pandey <exploretheuniverse216@gmail.com>
2025-06-01 12:52:23 +0530

    Added show more button functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useState,useEffect } from 'react';
 import ListingItem from '../components/ListingItem';
+import { url } from 'inspector';
 export default function Search() {
     const navigate = useNavigate();
     const [sidebardata, setSidebardata] = useState({
@@ -16,7 +17,11 @@ export default function Search() {
 
 const[loading, setLoading] = useState(false);
 const [listings, setListings] = useState([]);
-console.log(listings);
+const [showMore, setShowMore] = useState(false);
+
+
+
+
 useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get('searchTerm');
@@ -49,9 +54,16 @@ useEffect(() => {
 
     const fetchListings = async () => {
       setLoading(true);
+      setShowMore(false);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+      if(data.length>8){
+        setShowMore(true);
+      }
+      else{
+        setShowMore(false);
+      }
       setListings(data);
       setLoading(false);
     };
@@ -103,6 +115,20 @@ const handleChange = (e) => {
     urlParams.set('order', sidebardata.order);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex= numberOfListings ;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('start', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if(data.length <9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
   };
   return (
     <div className='flex flex-col md:flex-row'>
@@ -226,6 +252,16 @@ const handleChange = (e) => {
             listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
             ))}
+
+          {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className='text-color-green-700 hover hover-underline p-4 text-center w-full'
+            >
+              Show More
+            </button>
+          )}
+
         </div>
 
       </div>
